7b27f5468982e1935ef85d039a0e201c881b53cd
KJW519 <jwkang519@gmail.com>
2024-03-18 20:49:57 +0900

    add offset and hovered

--- src/Components/Web/CardPage/Cards.js
+++ src/Components/Web/CardPage/Cards.js
@@ -1,27 +1,46 @@
-import React from "react";
+import React, {useRef, useState} from 'react';
 import styled from "styled-components";
 import Minju from "../../../Assets/img/Minju.png"
 
 const Container = styled.div `
     width: 220px;
     height: 310px;
+    position: relative;
 `;
 
 const Card = styled.div `
     width: 500px;
-    height: 310px;
+    height: 300px;
     background-image: url(${Minju});
     background-size: cover;
     transition : all 0.3s ease-in-out;
-    `; 
+    `;
 
 const CardPage = () => {
-    const handleMouseOver =() => {
-        Container.style = 'transform : rotateY(20deg);'
+    const [offset, setOffset] = useState({x: 0, y: 0});
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleMouseMove = (event) => {
+        const {offsetX, offsetY} = event.nativeEvent;
+        setOffset({x: offsetX, y: offsetY});
     };
+
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+      };
+    
+      const handleMouseLeave = () => {
+        setIsHovered(false);
+        setOffset({x: 0, y: 0})
+      };
+
     return (
-        <Container>
-            <Card></Card>
+        <Container onMouseMove={handleMouseMove} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+            <Card offsetX={offset.x} offsetY={offset.y}/>
+
+            <p>Mouse X Offset: {offset.x}</p>
+            <p>Mouse Y Offset: {offset.y}</p>
+            <p>Is Hovered: {isHovered ? 'Yes' : 'No'}</p>
         </Container>
     );
 

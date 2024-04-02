83b25e34c7bbc462d9f8bc7e22f47551673099cd
KJW519 <jwkang519@gmail.com>
2024-04-03 00:51:51 +0900

    Multiple Cards

--- src/Components/Web/CardPage/Cards.js
+++ src/Components/Web/CardPage/Cards.js
@@ -1,32 +1,39 @@
 import React, { useState } from 'react';
 import styled from "styled-components";
 import Minju from "../../../Assets/img/Minju.png";
+import { Link } from "react-router-dom";
+import Card1 from "../../../Assets/img/Card.jpg";
+
+const Background = styled.div`
+    background-color: black;
+`;
 
 // 카드들을 담을 컨테이너 스타일링
 const Container = styled.div`
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
+   
 `;
 
 // 각각의 카드를 담을 스타일링된 컨테이너
 const CardContainer = styled.div`
-    width: 220px;
-    height: 310px;
-    perspective: 350px; 
+    width: 400px;
+    height: 500px;
+    perspective: 600px; 
     text-align: center;
-    margin: 20px;
+    margin: 50px;
 `;
 
 // 실제 카드의 스타일링
 const Card = styled.div`
     width: 100%;
     height: 100%;
-    background-image: url(${Minju});
+    background-image: url(${Minju}); /* props로 전달된 이미지 URL 사용 */
     background-size: cover;
+    border-radius: 10px;
     transition: transform 0.1s ease-in-out; 
     transform-style: preserve-3d; 
-    // offsetX, offsetY에 따라 회전 각도 설정, isHovered에 따라 hover 상태일 때만 회전
     transform: ${props => props.isHovered ? `rotateY(${ -1/5 * props.offsetX + 20 }deg) rotateX(${4/30 * props.offsetY - 20 }deg)` : 'none'};
 `;
 
@@ -34,8 +41,12 @@ const Card = styled.div`
 const CardPage = () => {
     // 카드들의 상태를 관리하는 state
     const [cards, setCards] = useState([
-        { id: 1, offsetX: 0, offsetY: 0, isHovered: false },
-        { id: 2, offsetX: 0, offsetY: 0, isHovered: false },
+        { id: 1, offsetX: 0, offsetY: 0, isHovered: false,  },
+        { id: 2, offsetX: 0, offsetY: 0, isHovered: false,  },
+        { id: 3, offsetX: 0, offsetY: 0, isHovered: false },
+        { id: 4, offsetX: 0, offsetY: 0, isHovered: false },
+        { id: 5, offsetX: 0, offsetY: 0, isHovered: false },
+        { id: 6, offsetX: 0, offsetY: 0, isHovered: false },
         // 필요한 만큼 카드 추가 가능
     ]);
 
@@ -62,15 +73,19 @@ const CardPage = () => {
 
     // 마우스 이탈 시 호출되는 함수, isHovered 상태를 false로 업데이트하고 offsetX, offsetY를 초기화
     const handleMouseLeave = (index) => {
-        setCards(prevCards => prevCards.map((card, i) => {
-            if (i === index) {
-                return { ...card, isHovered: false};
-            }
-            return card;
-        }));
+        setTimeout(() => { // setTimeout을 사용하여 일정 시간 후에 회전 각도를 초기화합니다.
+            setCards(prevCards => prevCards.map((card, i) => {
+                if (i === index) {
+                    return { ...card, isHovered: false, offsetX: 0, offsetY: 0};
+                }
+                return card;
+            }));
+        }, 500); // 500ms 후에 초기화되도록 설정합니다.
     };
 
     return (
+        <Background>
+            <Link to="/">Home</Link>
         <Container>
             {/* cards 배열을 map 함수를 사용하여 각각의 카드를 생성 */}
             {cards.map((card, index) => (
@@ -79,12 +94,10 @@ const CardPage = () => {
                     <Card offsetX={card.offsetX} offsetY={card.offsetY} isHovered={card.isHovered} 
                           onMouseEnter={() => handleMouseEnter(index)} 
                           onMouseLeave={() => handleMouseLeave(index)} />
-                    <p>Mouse X Offset: {card.offsetX}</p>
-                    <p>Mouse Y Offset: {card.offsetY}</p>
-                    <p>Is Hovered: {card.isHovered ? 'Yes' : 'No'}</p>
                 </CardContainer>
             ))}
         </Container>
+        </Background>
     );
 };
 
